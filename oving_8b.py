3fc49d12e2f995b544f0019c760cfb24aed43a1a
philipshahdadfar <93126708+philipshahdadfar@users.noreply.github.com>
2021-10-31 14:53:33 +0100

    Update oving_8b.py

--- oving_8b.py
+++ oving_8b.py
@@ -1,86 +1,125 @@
-with open("sporsmaalsfil.txt", "r", encoding="UTF8") as file:
 
-    class Flervalgssporsmaal:
-        
-        def __init__(self, sporsmaal, riktig_svar, valg):
-            self.__sporsmaal = sporsmaal
-            self.__valg = valg
-            self.__riktig_svar = riktig_svar
-        
-        @property
-        def sporsmaal(self):
-            return self.__sporsmaal
-        @property
-        def valg(self):
-            return self.__valg
-        @property
-        def riktig_svar(self):
-            return self.__riktig_svar
-      
+
+class Flervalgssporsmaal:
     
+    def __init__(self, sporsmaal, riktig_svar, valg):
+        self.__sporsmaal = sporsmaal
+        self.__valg = valg
+        self.__riktig_svar = riktig_svar
+    
+    @property
+    def sporsmaal(self):
+        return self.__sporsmaal
+    @property
+    def valg(self):
+        return self.__valg
+    @property
+    def riktig_svar(self):
+        return self.__riktig_svar
+  
+
+    
+    
+    def sjekk_svar(self, svaret):
+        if svaret == self.riktig_svar:
+            return True
+        else:
+            return False
+        
+    def __str__(self):
+        tekst = "Spørsmål:  " 
+        tekst += self.sporsmaal + "\n"
+        
+        for valg_nr, svar in enumerate(self.valg):
+            tekst += f"{valg_nr}: {svar} \n"
+            
+        return tekst
+    
+    def korrekt_svar_tekst(self):
+        tekst = "Korrekt svar!"
+        
+        return tekst
         
         
-        def sjekk_svar(self, svaret):
-            if svaret == self.riktig_svar:
-                return True
-            else:
-                return False
+        
+
+
+    
+    
+    
+
+        
+
+        
+    
+   
+
+
+
+
+def lag_sporsmaal():
+    sporsmaalene = []
+    
+    file = open("sporsmaalsfil.txt", "r", encoding="UTF8")
+
+    for line in file:
+    
+        stripped_line = line.strip()
+    
+        stripped_line = stripped_line.replace("[","")
+        stripped_line = stripped_line.replace("]","")
             
-        def __str__(self):
-            tekst = "Spørsmål:  " 
-            tekst += self.sporsmaal + "\n"
+        split_line = stripped_line.split(":")
             
-            for valg_nr, svar in enumerate(self.valg):
-                tekst += f"{valg_nr}: {svar} \n"
+        #print(split_line)
                 
-            return tekst
         
-        def sporsmaal_fra_fil():
-            
-            
-            for line in file:
-                
-                stripped_line = line.strip()
-            
-                split_line = stripped_line.split(":")
-            
-                #print(split_line)
-                
-                question = split_line[0]
+        question = split_line[0]
         
-                answer = split_line[1]
+        answer = int(split_line[1])
         
-                options = split_line[2]
-                
-        
-        
-            
-            
-            
+        options = split_line[2]
+    
+        opt1 = options.split(",")
     
     
-    def lag_sporsmaal():
-        sporsmaalene = []
-        sporsmaalene.append(Flervalgssporsmaal("Hvor ligger UiS?", 1, 
-                                      ["Bergen", "Stavanger", "Oslo", "Kristiansand"]))
-        sporsmaalene.append(Flervalgssporsmaal("Hva er 2+2?", 3,
-                                      ["1", "2", "3", "4"]))
-        
-        return sporsmaalene
+        sporsmaalene.append(Flervalgssporsmaal(question, answer, opt1))
+    
+    
+    
+    return sporsmaalene
 
 
+
+
+
+if __name__ == "__main__":
+    sporsmaalene = lag_sporsmaal()
+    riktig_svar = 0
+    for sporsmaal in sporsmaalene:
+        print(sporsmaal)
+        svar = input("Skriv inn svaret ditt: ")
+        svar = int(svar)
+        if sporsmaal.sjekk_svar(svar):
+            print("Riktig!\n")
+            riktig_svar += 1
+        else:
+            print("Feil!")
+    print(f"{riktig_svar} riktige av {len(sporsmaalene)}.")
 
 
     
+        
+   
+        
 
 
-            
-    
+
+  
+
         
     
-        
-        
-      
+       
         
     
         
@@ -91,41 +130,6 @@ with open("sporsmaalsfil.txt", "r", encoding="UTF8") as file:
         
        
                     
-                
-                
-                
-              
-                
- 
-                
-                
-            
-            
-            
-    
-        
-
-        
-        
-        
-        
-        
-        
-    
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-    
-    
+      
     
     
